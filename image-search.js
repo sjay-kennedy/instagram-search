4425ee534fc0b1e58ff91d9fe7a17ffeecd146fc
shannon_k <shannon_k@dell.com>
2015-05-14 10:50:34 -0500

    styled the page

--- image-search.js
+++ image-search.js
@@ -1,7 +1,15 @@
 angular.module('instagramSearch', [])
+
 .controller('imageSearch', function($scope, $timeout, $q, $http) {
     $scope.getImages = function(tag, nextUrl) {
-        $scope.loaded=false;
+        function wait() {
+            var defer = $q.defer();
+            $timeout(function() {
+                defer.resolve();
+            }, 1000);
+            return defer.promise;
+        }
+        $scope.loaded='loading';
         var request = {
             client_id: "42205d095a124d65a29b630908989b27",
             callback: "JSON_CALLBACK",
@@ -10,22 +18,26 @@ angular.module('instagramSearch', [])
         if (nextUrl) {
             var url = nextUrl
         }else {
-            var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent?client_id=42205d095a124d65a29b630908989b27";
+            var url = "https://api.instagram.com/v1/tags/" + tag + "/media/recent";
         }
         $http({
             method: 'JSONP',
             url: url,
             params: request
-        }).
-        success(function(result) {
-            $scope.loaded = true;
+        })
+        .success(function(result) {
             $scope.result = result;
             $scope.images = $scope.result.data;
             $scope.next = $scope.result.pagination.next_url;
-            console.log('images', $scope.next);
-        }).
-        error(function() {
-            alert('error');
-        });
+                console.log('result', result);
+        })
+        .error(function() {
+                $scope.loaded = false;
+                $scope.errorMessage = "You won't find what you are looking for here."
+        })
+        .then(wait)
+        .then(function() {
+            $scope.loaded = true;
+        })
     };
 });
